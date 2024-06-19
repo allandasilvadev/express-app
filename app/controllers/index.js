fb0660f0b293b35997fb17f63498c628b5273f7a
Allan da Silva <allandasilva.dev@gmail.com>
2024-06-19 10:58:27 -0300

    feat: adiciona os testes para a view index

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,16 +1,15 @@
 const db = require('../../config/database')();
 
 module.exports = (app) => {
+    const Course = app.models.curso;
+
     return {
-        index: (req, res) => {
-            /*
-            res.json({
-                "message": "Application is running"
-            });
-            */
-            res.render('home/index', {
+        index: async (req, res) => {
+            var courses = await Course.find({}, ['name', 'category', 'price'], { sort: { price: 1 } }).exec();
+
+            return res.render('home/index', {
                 title: 'Hi, friends',
-                courses: db.courses
+                courses: courses.map(course => course.toObject())
             });
         }
     }
--- tests/ui/home.spec.js
+++ tests/ui/home.spec.js
@@ -1,9 +1,36 @@
 const request = require('supertest');
 const app = require('../../config/express')();
+const Course = app.models.curso;
+
+const Utils = require('../utils/db');
 
 describe('Routes: index', () => {
 
+    beforeAll(async () => {
+        Utils.open_connection();
+    });
+    
+    beforeEach(async () => {
+        Utils.clear_database();
+
+        await Course.insertMany([
+            { name: 'Laravel 5', category: "Back-End", price: 270},
+            { name: 'Codeigniter 4', category: 'Back-End', price: 420}
+        ]);
+    });
+
+    afterEach(async () => {
+        Utils.clear_database();
+    });
+    
+    afterAll(async () => {
+        Utils.close_connection();
+    });
+
     describe('GET /', () => {
+        afterEach(async () => {
+            Utils.clear_database();
+        });
 
         it('status 200', async () => {
             const response = await request(app).get('/').expect(200);
@@ -21,8 +48,8 @@ describe('Routes: index', () => {
             expect(response.text).toContain('Codeigniter 4');
             expect(response.text).toMatch(/R\$\s*420,00/);
 
-            expect(response.text).toContain('PHP 8');
-            expect(response.text).toMatch(/R\$\s*560,00/);
+            expect(response.text).toContain('Laravel 5');
+            expect(response.text).toMatch(/R\$\s*270,00/);
         });
 
     });
